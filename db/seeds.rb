3f42451864d90884eb231772f2c36b87218a80af
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-01-02 12:07:11 -0800

    write db/seeds.rb and seed with some cats

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+#Cats (:name, :birth_date, :color, :sex, :description)
+c1 = Cat.create(name: 'JJ', birth_date: '2008-07-15', color: 'black', sex: 'M', description: 'He is a bit grumpy.')
+c2 = Cat.create(name: 'Meowgret Thatcher', birth_date: '2009-04-22', color: 'gray', sex: 'F', description: 'AKA Kitten; she is a pretty girl.')
+c3 = Cat.create(name: 'Monks', birth_date: '2011-09-21', color: 'black', sex: 'M', description: 'He is sweet but naughty.')
