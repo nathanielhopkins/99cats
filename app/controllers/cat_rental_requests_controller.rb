fe26a56a470a96ab484780b0e19b31bbd5f7f8a0
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-01-05 12:25:40 -0800

    write cat_rental_request_params helper method and #create in CatRentalRequestsController

--- app/controllers/cat_rental_requests_controller.rb
+++ app/controllers/cat_rental_requests_controller.rb
@@ -3,4 +3,20 @@ class CatRentalRequestsController < ApplicationController
     @cat_rental_request = CatRentalRequest.new
     render :new
   end
+
+  def create
+    @cat_rental_request = CatRentalRequest.new(cat_rental_request_params)
+
+    if @cat_rental_request.save
+      redirect_to cat_rental_request_url(@cat_rental_request)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def cat_rental_request_params
+    params.require(:cat_rental_request).permit(:cat_id, :start_date, :end_date)
+  end
 end
