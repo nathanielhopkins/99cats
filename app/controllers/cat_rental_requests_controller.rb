b79e954a19017a31f08727421ba8c083edd80c6a
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-01-05 12:27:36 -0800

    change #create in CatRentalRequestsController to redirect to cat after successful save

--- app/controllers/cat_rental_requests_controller.rb
+++ app/controllers/cat_rental_requests_controller.rb
@@ -8,7 +8,7 @@ class CatRentalRequestsController < ApplicationController
     @cat_rental_request = CatRentalRequest.new(cat_rental_request_params)
 
     if @cat_rental_request.save
-      redirect_to cat_rental_request_url(@cat_rental_request)
+      redirect_to cat_url(@cat_rental_request.cat_id)
     else
       render :new
     end
